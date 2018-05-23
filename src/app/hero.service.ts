afd3410ef758c331c79493c735189a266e31212b
Miguel Aguila <m.aguila@solcloud.cl>
2018-05-22 20:22:23 -0400

    http client primera version

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
@@ -9,16 +11,25 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService : MessageService) { }
+  private heroesUrl = 'http://localhost:3000/heroes';  // URL to web api
+
+  constructor(private http: HttpClient,
+              private messageService : MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    //return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
-  }    
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
 }
